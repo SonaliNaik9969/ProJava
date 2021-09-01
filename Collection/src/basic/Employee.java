e19608593395a8e7afb0394f2c41df27ff224ceb
SonaliNaik9969 <sonalinaik1865@gmail.com>
2021-09-01 15:43:55 +0530

    core java projects

---
+++ Collection/src/basic/Employee.java
@@ -0,0 +1,51 @@
+package basic;
+
+public class Employee {
+	
+	private Long id;
+	private String name;
+	private Integer Salary;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(Long id, String name, Integer salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		Salary = salary;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getSalary() {
+		return Salary;
+	}
+
+	public void setSalary(Integer salary) {
+		Salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", Salary=" + Salary + "]";
+	}
+	
+	
+
+}
